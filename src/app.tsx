018ce3da8ba8729c7df8f0a3552d0e9643214239
zhicheng ran <307853639@qq.com>
2023-04-27 17:12:49 +0800

    init

--- src/app.tsx
+++ src/app.tsx
@@ -1,65 +1,32 @@
-import { Component, PropsWithChildren } from "react";
-import sr from "sr-sdk-wxapp";
-import { Text } from "@tarojs/components";
+import React, { useEffect } from "react";
 
+// Taro 额外添加的 hooks 要从 '@tarojs/taro' 中引入
+import { useDidShow, useDidHide } from "@tarojs/taro";
+
+// 假设我们要使用 Redux
+
+// 全局样式
 import "./app.less";
+import { Button } from "@tarojs/components";
 
-/**
- * 有数埋点SDK 默认配置
- * 使用方法请参考文档 https://mp.zhls.qq.com/youshu-docs/develop/sdk/Taro.html
- * 如对有数SDK埋点接入有任何疑问，请联系微信：sr_data_service
- */
-sr.init({
-  /**
-   * 有数 - ka‘接入测试用’ 分配的 app_id，对应的业务接口人负责
-   */
-  token: "bi6cdbda95ae2640ec",
+function App(props) {
+  // 可以使用所有的 React Hooks
+  useEffect(() => {});
 
-  /**
-   * 微信小程序appID，以wx开头
-   */
-  appid: "touristappid",
+  // 对应 onShow
+  useDidShow(() => {});
 
-  /**
-   * 如果使用了小程序插件，需要设置为 true
-   */
-  usePlugin: false,
+  // 对应 onHide
+  useDidHide(() => {});
 
-  /**
-   * 开启打印调试信息， 默认 false
-   */
-  debug: true,
-
-  /**
-   * 建议开启-开启自动代理 Page， 默认 false
-   * sdk 负责上报页面的 browse 、leave、share 等事件
-   * 可以使用 sr.page 代替 Page(sr.page(options))
-   * 元素事件跟踪，需要配合 autoTrack: true
-   */
-  proxyPage: true,
-  /**
-   * 建议开启-开启组件自动代理， 默认 false
-   * sdk 负责上报页面的 browse 、leave、share 等事件
-   */
-  proxyComponent: true,
-  // 建议开启-是否开启页面分享链路自动跟踪
-  openSdkShareDepth: true,
-  // 建议开启-元素事件跟踪，自动上报元素事件，入tap、change、longpress、confirm
-  autoTrack: true,
-  installFrom: "Taro@v3",
-});
-
-class App extends Component<PropsWithChildren> {
-  componentDidMount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  // this.props.children 是将要会渲染的页面
-  render() {
-    return <>{this.props.children}</>;
-  }
+  return (
+    // 在入口组件不会渲染任何内容，但我们可以在这里做类似于状态管理的事情
+    <>
+      {/* props.children 是将要被渲染的页面 */}
+      {props.children}
+      <Button>test</Button>
+    </>
+  );
 }
 
 export default App;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,21 +1,13 @@
 import { View, Text } from "@tarojs/components";
-import { useShareAppMessage } from "@tarojs/taro";
 
-// page.js
-export default function Index() {
-  useShareAppMessage((res) => {
-    if (res.from === "button") {
-      // 来自页面内转发按钮
-      console.log(res.target);
-    }
-    return {
-      title: "自定义转发标题",
-      path: "/page/user?id=123",
-    };
-  });
+const Index: React.FC<AppProps> = () => {
   return (
-    <View className="me">
-      <Text>Hello world!</Text>
+    <View className="index">
+      <Text>Index</Text>
     </View>
   );
-}
+};
+
+interface AppProps {}
+export default Index;
+Index.displayName = "Index";
